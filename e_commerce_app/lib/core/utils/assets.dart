4c9307a98cabb762a910ced093ff8741fd3a061a
AbdelrhmanNashaat <abdelrhman.nashaat22@gmail.com>
2023-12-22 11:03:49 +0200

    azkar app

--- e_commerce_app/lib/core/utils/assets.dart
+++
@@ -1,9 +0,0 @@
-abstract class AssetsData {
-  static const splashImage = 'assets/images/Vector.png';
-  static const pageViewImage2 = 'assets/images/Online shopping-rafiki 1.png';
-  static const pageViewImage1 = 'assets/images/Online shopping-pana 1.png';
-  static const pageViewImage3 = 'assets/images/Credit Card Payment-cuate 1.png';
-  static const facebookLogo = 'assets/images/vuvietduc.com.png';
-  static const googleLogo = 'assets/images/Group.png';
-  static const arrowBack = 'assets/images/arrow.png';
-}
--- routine_habit_tracker_app/android/app/src/main/kotlin/com/example/routine_habit_tracker_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.routine_habit_tracker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
